028141377d34bb302e15f2d2d99ddf139a3874ce
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-02-17 17:43:20 +0300

    feat: organized the structure of the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+
+import App from './components/App/App.tsx';
+import ErrorPage from './pages/errorPage/error-page.tsx';
+import Home from './pages/Home/index.tsx';
+
+import './styles/index.scss';
 import store from './redux/store.ts';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
---
+++ src/pages/errorPage/error-page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+const errorPage = () => {
+  return (
+    <div className="errorPage">
+      <h1>Упс!</h1>
+      <p>Страница не найдена</p>
+    </div>
+  );
+};
+export default errorPage;
